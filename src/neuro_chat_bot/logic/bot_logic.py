3adda057f5b76c4de610499d606c2554c3ab5ba4
Sergey Kazantsev <serzh.kazantseff@gmail.com>
2024-04-26 18:40:54 +0500

    Поправил сигнатуру логики

--- src/neuro_chat_bot/logic/bot_logic.py
+++ src/neuro_chat_bot/logic/bot_logic.py
@@ -1,4 +1,3 @@
-# Запуск чат-бота и получение ответа
 import json
 import search_answer
 
@@ -10,31 +9,33 @@ class BotLogic:
     Логика для чат-бота
     """
 
-    def __init__(self, message: str) -> None:
+    def __init__(self) -> None:
         """
-        Инициализирует объект BotLogic.
-
-        :param message: Текст сообщения
-        :type message: str
+        Инициализирует логику для чат-бота.
         """
-        self.message = message
         with open(INTENSE_FILE_PATH) as intense_file:
             self.intents = json.loads(intense_file.read())
 
-    def handle_message(self) -> str:
+    def handle_message(self, message) -> str:
         """
-        Обрабатывает сообщение, которое пришло из чат-бота
+        Отвечает на сообщение, которое пришло из чат-бота
+
+        :param message: Текст сообщения
+        :type message: str
 
         :return: Текст сообщения, которое будет выведено пользователю в ответ
         """
-        ints = search_answer.predict_class(self.message)
+        ints = search_answer.predict_class(message)
         answer = search_answer.get_response(ints, self.intents)
         return answer
 
 
-print("Chatbot is up!")
+# Для тестирования логики бота. Перед этим необходимо запустить создание модели (training.py)
+if __name__ == "__main__":
+    logic = BotLogic()
+    print("Chatbot is up!")
 
-while True:
-    message = input("")
-    result = BotLogic(message).handle_message()
-    print(result)
+    while True:
+        message = input("")
+        result = logic.handle_message(message)
+        print(result)
